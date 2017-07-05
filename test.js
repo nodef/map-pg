9de5d3293e416ae3a7a86fb6f946bf3c2efde8fa
Subhajit Sahu <wolfram77@gmail.com>
2017-07-05 19:18:06 +0530

    test: if-then is not a single statement?

--- test.js
+++ test.js
@@ -9,17 +9,6 @@ var url = process.env.DATABASE_URL;
 pg.defaults.ssl = true;
 
 
-// tests
-var tsize = function(m) {
-	m.size.then((n) => {
-		console.log();
-		if(n!==0) throw new RangeError('cleared map size should be 0');
-	});
-	for(var i=0, I=Math.floor(26*Math.random()); i<I; i++) {
-		m.set();
-	}
-};
-
 // test map
 console.log('pg.connect: '+url);
 var pool = new pg.Pool(pgconfig(url));
@@ -30,16 +19,16 @@ pool.connect(function(err, db, done) {
 	var m = new Map();
 	var M = new mappg(db);
 	M.clear();
-	M.size.then((n) => if(m.size!==n) throw new Error('size mismatch'));
+	M.size.then((n) => { if(m.size!==n) throw new Error('size mismatch'); });
 	for(var i=0, I=26*Math.random(); i<I; i++) {
 		m.set('a'+i, ''+i);
 		P = M.set('a'+i, ''+i);
 	}
 	P.then(() => {
-		M.size.then((n) => if(m.size!==n) throw new Error('size mismatch'));
+		M.size.then((n) => { if(m.size!==n) throw new Error('size mismatch'); });
 		for(var i=0; i<26; i++) {
-			M.has('a'+i).then((v) => if(m.has('a'+i)!==v) throw new Error('has mismatch'));
-			M.get('a'+i).then((v) => if(m.get('a'+i)!==v) throw new Error('get mismatch'));
+			M.has('a'+i).then((v) => { if(m.has('a'+i)!==v) throw new Error('has mismatch'); });
+			M.get('a'+i).then((v) => { if(m.get('a'+i)!==v) throw new Error('get mismatch'); });
 		}
 	});
 	for(var i=0, I=26*Math.random(); i<I; i++) {
@@ -47,18 +36,18 @@ pool.connect(function(err, db, done) {
 		P = M.delete('a'+i);
 	}
 	P.then(() => {
-		M.size.then((n) => if(m.size!==n) throw new Error('size mismatch'));
+		M.size.then((n) => { if(m.size!==n) throw new Error('size mismatch'); });
 		for(var i=0; i<26; i++) {
-			M.has('a'+i).then((v) => if(m.has('a'+i)!==v) throw new Error('has mismatch'));
-			M.get('a'+i).then((v) => if(m.get('a'+i)!==v) throw new Error('get mismatch'));
+			M.has('a'+i).then((v) => { if(m.has('a'+i)!==v) throw new Error('has mismatch'); });
+			M.get('a'+i).then((v) => { if(m.get('a'+i)!==v) throw new Error('get mismatch'); });
 		}
 	});
 	M.forEach((v, k) => {
 		if(m.get(k)!==v) throw new Error('forEach mismatch');
 	});
-	M.valueOf((v) => if(!_.isEqual(m, v)) throw new Error('valueOf mismatch'));
-	M.entries((v) => if(!_.isEqual(m.entries(), v)) throw new Error('entries mismatch'));
-	M.keys((v) => if(!_.isEqual(m.keys(), v)) throw new Error('keys mismatch'));
-	M.values((v) => if(!_.isEqual(m.values(), v)) throw new Error('values mismatch'));
+	M.valueOf((v) => { if(!_.isEqual(m, v)) throw new Error('valueOf mismatch'); });
+	M.entries((v) => { if(!_.isEqual(m.entries(), v)) throw new Error('entries mismatch'); });
+	M.keys((v) => { if(!_.isEqual(m.keys(), v)) throw new Error('keys mismatch'); });
+	M.values((v) => { if(!_.isEqual(m.values(), v)) throw new Error('values mismatch'); });
 	done();
 });
