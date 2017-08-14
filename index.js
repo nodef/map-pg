11c3b87a74cc4a1ef98dc0113a4717fd4aa429ca
Subhajit Sahu <wolfram77@gmail.com>
2017-08-14 19:01:44 +0530

    :wc: let me use my skills

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ var $ = function(db, tab, key, val) {
 	this.tab = tab || 'MAP';
 	this.key = key || 'key';
 	this.val = val || 'val';
-	this.db.query(`CREATE TABLE IF NOT EXISTS "${this.tab}"("${this.key}" TEXT PRIMARY KEY, "${this.val}" TEXT)`, (err, res) => {
-		if(err) throw err;
-	});
+	this.db.query(`CREATE TABLE IF NOT EXISTS "${this.tab}"("${this.key}" TEXT PRIMARY KEY, "${this.val}" TEXT)`;
 };
 module.exports = $;
 
@@ -14,114 +12,85 @@ var _ = $.prototype;
 
 
 Object.defineProperty(_, 'size', {'get': function() {
-	return new Promise((fres, frej) => {
-		this.db.query(`SELECT COUNT(*) AS cnt FROM "${this.tab}"`, (err, res) => {
-			if(err) frej(err);
-			else fres(parseInt(res.rows[0].cnt));
-		});
-	});
+	return this.db.query(
+		`SELECT COUNT(*) AS n FROM ${this.tab};`
+	).then((ans) => parseInt(ans.rows[0].n));
 }});
 
 
 _.has = function(k) {
-	return new Promise((fres, frej) => {
-		this.db.query(`SELECT "${this.key}" FROM "${this.tab}" WHERE "${this.key}"=$1`, [k], (err, res) => {
-			if(err) frej(err);
-			else fres(res.rowCount===1);
-		});
-	});
+	return this.db.query(
+		`SELECT "${this.key}" FROM "${this.tab}" `+
+		`WHERE "${this.key}"=$1;`, [k]
+	).then((ans) => ans.rowCount===1);
 };
 
 
 _.get = function(k) {
-	return new Promise((fres, frej) => {
-		this.db.query(`SELECT "${this.val}" AS val FROM "${this.tab}" WHERE "${this.key}"=$1`, [k], (err, res) => {
-			if(err) frej(err);
-			else fres(res.rows.length>0? res.rows[0].val : undefined);
-		});
-	});
+	return this.db.query(
+		`SELECT "${this.val}" AS v FROM "${this.tab}" `+
+		`WHERE "${this.key}"=$1;`, [k]
+	).then((ans) => ans.rowCount? ans.rows[0].v : undefined);
 };
 
 
 _.set = function(k, v) {
-	return new Promise((fres, frej) => {
-		this.db.query(`INSERT INTO "${this.tab}" ("${this.key}", "${this.val}") VALUES ($1, $2) ON CONFLICT ("${this.key}") DO UPDATE SET "${this.val}" = $2`, [k, v], (err, res) => {
-			if(err) frej(err);
-			else fres(res.rowCount);
-		});
-	});
+	return this.db.query(
+		`INSERT INTO "${this.tab}" ("${this.key}", "${this.val}") `+
+		`VALUES ($1, $2) ON CONFLICT ("${this.key}") `+
+		`DO UPDATE SET "${this.val}"=$2;`, [k, v]
+	).then((ans) => ans.rowCount);
 };
 
 
 _.delete = function(k) {
-	return new Promise((fres, frej) => {
-		this.db.query(`DELETE FROM "${this.tab}" WHERE "${this.key}"=$1`, [k], (err, res) => {
-			if(err) frej(err);
-			else fres(res.rowCount);
-		});
-	});
+	return this.db.query(
+		`DELETE FROM "${this.tab}" `+
+		`WHERE "${this.key}"=$1;`, [k]
+	).then((ans) => ans.rowCount);
 };
 
 
 _.clear = function() {
-	return new Promise((fres, frej) => {
-		this.db.query(`DELETE FROM "${this.tab}"`, (err, res) => {
-			if(err) frej(err);
-			else fres(res.rowCount);
-		});
-	});
-};
-
-
-_.forEach = function(fn, thisArg) {
-	return new Promise((fres, frej) => {
-		this.db.query(`SELECT "${this.key}" AS key, "${this.val}" AS val FROM "${this.tab}"`, (err, res) => {
-			if(err) frej(err);
-			for(var i=0, I=res.rowCount; i<I; i++) {
-				fn.call(thisArg, res.rows[i].val, res.rows[i].key);
-			}
-			fres(res.rowCount);
-		});
-	});
+	return this.db.query(
+		`DELETE FROM "${this.tab}";`
+	).then((ans) => ans.rowCount);
 };
 
 
 _.valueOf = function() {
-	return new Promise((fres, frej) => {
-		this.db.query(`SELECT "${this.key}" AS key, "${this.val}" AS val FROM "${this.tab}"`, (err, res) => {
-			if(err) frej(err);
-			var a = new Map();
-			for(var i=0, I=res.rowCount; i<I; i++) {
-				a.set(res.rows[i].key, res.rows[i].val);
-			}
-			fres(a);
-		});
+	return this.db.query(
+		`SELECT "${this.key}" AS k, "${this.val}" AS v FROM "${this.tab}";`
+	).then((ans) => {
+		var a = new Map();
+		for(var i=0, I=res.rowCount; i<I; i++)
+			a.set(ans.rows[i].k, ans.rows[i].v);
+		return a;
 	});
 };
 
 
 _.entries = function() {
-	return new Promise((fres, frej) => {
-		var v = this.valueOf();
-		v.then((a) => { fres(a.entries()); });
-		v.catch((err) => { frej(err); });
-	});
+	return this.valueOf().then((ans) => ans.entries());
 };
 
 
 _.keys = function() {
-	return new Promise((fres, frej) => {
-		var v = this.valueOf();
-		v.then((a) => { fres(a.keys()); });
-		v.catch((err) => { frej(err); });
-	});
+	return this.valueOf().then((ans) => ans.keys());
 };
 
 
 _.values = function() {
-	return new Promise((fres, frej) => {
-		var v = this.valueOf();
-		v.then((a) => { fres(a.values()); });
-		v.catch((err) => { frej(err); });
+	return this.valueOf().then((ans) => ans.values());
+};
+
+
+_.forEach = function(fn, thisArg) {
+	return this.db.query(
+		`SELECT "${this.key}" AS k, "${this.val}" AS v FROM "${this.tab}"`
+	).then((ans) => {
+		for(var i=0, I=ans.rowCount; i<I; i++)
+			fn.call(thisArg, ans.rows[i].v, ans.rows[i].k);
+		return ans.rowCount;
 	});
 };
