065a8bf1338ea022706e8333765afd1809f99ce7
Subhajit Sahu <wolfram77@gmail.com>
2017-09-11 02:17:04 +0530

    :wc: new test

--- test.js
+++ test.js
@@ -1,70 +1,39 @@
+var pgconfig = require('pg-connection-string').parse;
 var assert = require('assert');
 var MapPg = require('./');
 var pg = require('pg');
-var pgconfig = require('pg-connection-string').parse;
 
 var pool = new pg.Pool(pgconfig(process.env.DATABASE_URL));
+var type = {
+  "key": "TEXT",
+  "value": "TEXT"
+};
 pool.connect((err, db, done) => {
-  var whypa = new MapPg(db, 'why', {'q': 'text', 'a': 'text'}, 'q', 'a');
-  var whypb = new MapPg(db, 'why', {'q': 'text', 'a': 'text'}, 'q', ['a']);
-  var whypc = new MapPg(db, 'why', {'q': 'text', 'a': 'text'}, 'q', ['q', 'a']);
-  var whypd = new MapPg(db, 'why', {'q': 'text', 'a': 'text'}, ['q'], ['q', 'a']);
-  Promise.all([whypa, whypb, whypc, whypd]).then((why) => {
-    var whya = why[0], whyb = why[1], whyc = why[2], whyd = why[3];
+  var mapa = new MapPg(db);
+  var mapb = new MapPg(db, 'map', type, 'key', ['value']);
+  var mapc = new MapPg(db, 'map', type, 'key', ['key', 'value']);
+  var mapd = new MapPg(db, 'map', tyoe, ['key'], ['key', 'value']);
+  mapa.setup().then(() => console.log('Table created'));
 
-    whya.set('Why is the sky blue, yellow, red, black and white?', 'Because your eyes are painted on the inside.').then((ans) => {
-      assert.equal(ans, 1);
-    });
-    whyb.set('What is that big pipe?', {'a': 'Its called postbox and it is where you came from.'}).then((ans) => {
-      assert.equal(ans, 1);
-    });
-    whyc.set('Why are you so happy at the end of every month?', {'a': 'Because there are 2 moons on that night.'}).then((ans) => {
-      assert.equal(ans, 1);
-    });
-    whyd.set({'q': 'Why so serious?'}, {'a': 'You are joker?'}).then((ans) => {
-      assert.equal(ans, 1);
-    });
-    
-    whya.size.then((ans) => {
-      assert.equal(ans, 4);
-    });
-    whyb.size.then((ans) => {
-      assert.equal(ans, 4);
-    });
-    whyc.size.then((ans) => {
-      assert.equal(ans, 4);
-    });
-    whyd.size.then((ans) => {
-      assert.equal(ans, 4);
-    });
-    
-    whya.get('Why so serious?').then((ans) => {
-      assert.equal(ans, 'You are joker?');
-    });
-    whyb.get('Why are you so happy at the end of every month?').then((ans) => {
-      assert.deepEqual(ans, {'a': 'Because there are 2 moons on that night.'});
-    });
-    whyc.get('What is that big pipe?').then((ans) => {
-      assert.deepEqual(ans, {'q': 'What is that big pipe?', 'a': 'Its called postbox and it is where you came from.'});
-    });
-    whyd.get({'q': 'Why is the sky blue, yellow, red, black and white?'}).then((ans) => {
-      assert.deepEqual(ans, {'q': 'Why is the sky blue, yellow, red, black and white?', 'a': 'Because your eyes are painted on the inside.'});
-    });
-    whya.get('Hello Nanana').then((ans) => {
-      assert.equal(undefined);
-    });
-    
-    whya.delete('Why so serious?').then((ans) => {
-      assert.equal(ans, 1);
-    });
-    whyd.set({'q': 'Why are you so happy at the end of every month?'}, undefined).then((ans) => {
-      assert.deepEqual(ans, 1);
-    });
+  mapa.set('n', 'Noble');
+  mapb.set('p', {'value': 'Programming'});
+  mapc.set('m', {'value': 'Mantra'});
+  mapd.set('.', {'value': '2012'}).then(() => console.log('"." set'));
 
-    whya.size.then((ans) => {
-      assert.equal(ans, 2);
-    });
-    // ...
-  });
+  mapa.size.then((ans) => assert.equal(ans, 4));
+  mapb.size.then((ans) => assert.equal(ans, 4));
+  mapc.size.then((ans) => assert.equal(ans, 4));
+  mapd.size.then((ans) => assert.equal(ans, 4));
+
+  mapa.get('.').then((ans) => assert.equal(ans, '2012'));
+  mapb.get('m').then((ans) => assert.deepEqual(ans, {'value': 'Mantra'}));
+  mapc.get('p').then((ans) => assert.deepEqual(ans, {'key': 'p', 'value': 'Programming'}));
+  mapd.get({'key': 'n'}).then((ans) => assert.deepEqual(ans, {'key': 'n', 'value': 'Noble'}));
+  mapa.get('l').then((ans) => assert.equal(ans, undefined));
+
+  mapa.delete('m');
+  mapd.delete('x').then((ans) => assert.equal(ans, 0));
+
+  mapb.size.then((ans) => assert.equal(ans, 3));
   done();
 });
